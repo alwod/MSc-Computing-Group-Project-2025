e43cf67b270aa86f98ebe921b9e894519ebd5a1d
Mikha <mikharohrs@gmail.com>
2025-09-23 16:29:35 +0100

    Added variable

--- eclipse/src/mscGroupProjectEclipse/Test.java
+++ eclipse/src/mscGroupProjectEclipse/Test.java
@@ -1,11 +1,11 @@
 package mscGroupProjectEclipse;
+
 import javax.swing.JOptionPane;
 
 public class Test {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Hello World!");
-
+		String output = "Hello World!";
+		JOptionPane.showMessageDialog(null, output);
 	}
-
 }
