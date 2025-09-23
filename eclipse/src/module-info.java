38c517b61ce1e78932fe290b96e61aa2766c8361
Mikha <mikharohrs@gmail.com>
2025-09-23 13:59:49 +0100

    Initial Commit

---
+++ eclipse/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module mscGroupProjectEclipse {
+	requires java.desktop;
+}
---
+++ eclipse/src/mscGroupProjectEclipse/Test.java
@@ -0,0 +1,11 @@
+package mscGroupProjectEclipse;
+import javax.swing.JOptionPane;
+
+public class Test {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello World!");
+
+	}
+
+}
