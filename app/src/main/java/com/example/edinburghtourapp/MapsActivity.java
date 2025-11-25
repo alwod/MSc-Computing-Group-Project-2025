069c62222386a8ee28e9421a3565c2a20a03ea2b
Mikha <mikharohrs@gmail.com>
2025-11-25 13:38:09 +0000

    Partially completed routing

--- app/src/main/java/com/example/edinburghtourapp/MapsActivity.java
+++ app/src/main/java/com/example/edinburghtourapp/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.edinburghtourapp;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.annotation.SuppressLint;
@@ -12,6 +13,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.edinburghtourapp.databinding.ActivityMapsBinding;
@@ -47,16 +49,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-//        if (getIntent().getExtras() != null) {
-//            Tour tour = (Tour) getIntent().getSerializableExtra("testTour");
-//
-//            for (TourLocation tempLocation : tour.getTourLocations()) {
-//                markerPoints.add(tempLocation.getLatLng());
-//            }
-//        }
-
-        mMap.setMyLocationEnabled(true);
-    }
+    } // End of onCreate method
 
     private String getDirectionsUrl(LatLng origin, LatLng destination) {
         String str_origin = "origin=" + origin.latitude + ", " + origin.longitude;
@@ -76,7 +69,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         String output = "json";
         String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
         return url;
-    }
+    } // end of getDirectionsUrl method
 
     @SuppressLint("LongLogTag")
     private String downloadUrl(String strUrl) throws IOException {
@@ -105,7 +98,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         return data;
-    }
+    } // End of downloadUrl method
 
     private class DownloadTask extends AsyncTask<String, Void, String> {
         @Override
@@ -126,7 +119,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             parserTask.execute(result);
         }
-    }
+    } // end of Download Task class
 
     private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
         @Override
@@ -149,14 +142,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         @Override
         protected void onPostExecute(List<List<HashMap<String, String>>> result) {
-            ArrayList<LatLng> points = null;
-            PolylineOptions polylineOptions = null;
+            ArrayList<LatLng> points = new ArrayList<>();
+            PolylineOptions lineOptions = new PolylineOptions();
 
-            for (int index = 0; index < result.size(); index++) {
-                points = new ArrayList<LatLng>();
-                polylineOptions = new PolylineOptions();
+            for (int i = 0; i < result.size(); i++) {
 
-                List<HashMap<String, String>> path = result.get(index);
+                List<HashMap<String, String>> path = result.get(i);
 
                 for (int j = 0; j < path.size(); j++) {
                     HashMap<String, String> point = path.get(j);
@@ -168,36 +159,74 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     points.add(position);
                 }
 
-                polylineOptions.addAll(points);
-                polylineOptions.width(12);
-                polylineOptions.color(Color.RED);
             }
-            mMap.addPolyline(polylineOptions);
+
+            lineOptions.addAll(points);
+            lineOptions.width(12);
+            lineOptions.color(Color.RED);
+
+            // Drawing polyline in the Google Map for the entire route
+            mMap.addPolyline(lineOptions);
         }
-    }
+    } // End of ParserTask class
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16));
 
 //        mMap.addMarker(new MarkerOptions().position(markerPoints.get(0)).title("Marker"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(markerPoints.get(0)));
 
-    }
-}
+        // This function let's you click on the map to add points. Just doing this to test if routes work
+        handleMapClicking();
+
+    } // End of onMapReaady method
+
+
+    public void handleMapClicking() {
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+
+                // If there are already 2 markers on the map, clear the map.
+                if (markerPoints.size() > 1) {
+                    markerPoints.clear();
+                    mMap.clear();
+                }
+
+                // Add the marker, based on where you clicked
+                markerPoints.add(latLng);
+
+                MarkerOptions options = new MarkerOptions();
+                options.position(latLng);
+
+                // Starting marker will be green, the destination marker will be red.
+                if (markerPoints.size() == 1) {
+                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                } else if (markerPoints.size() == 2) {
+                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                }
+
+                mMap.addMarker(options);
+
+                // Make sure the marker's locations are captured
+                if (markerPoints.size() >= 2) {
+                    LatLng origin = (LatLng) markerPoints.get(0);
+                    LatLng dest = (LatLng) markerPoints.get(1);
+
+                    String url = getDirectionsUrl(origin, dest);
+
+                    DownloadTask downloadTask = new DownloadTask();
+
+                    downloadTask.execute(url);
+                }
+            }
+        });
+    } // end of handleMapClicking method
+
+} // End of class
