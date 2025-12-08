b381990bcf90477827fec65395a5a86d69921694
Prabhat Neupane <95750087+PrabhatNp@users.noreply.github.com>
2025-12-08 10:46:36 +0000

    Refactor TourMenuActivity for clarity and consistency

--- app/src/main/java/com/example/edinburghtourapp/TourMenuActivity.java
+++ app/src/main/java/com/example/edinburghtourapp/TourMenuActivity.java
@@ -1,35 +1,39 @@
 package com.example.edinburghtourapp;
 
-import androidx.activity.ComponentActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.ComponentActivity;
+
+// Tour selection screen
+
 public class TourMenuActivity extends ComponentActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour_menu);
 
-        // ---profile icon in Navigation bar  ---
+        //Profile icon in navigation bar
         ImageButton btnProfile = findViewById(R.id.btnProfile);
         btnProfile.setOnClickListener(v -> {
             Intent profileIntent = new Intent(this, ProfileActivity.class);
             startActivity(profileIntent);
         });
 
-        // --- Tour buttons ---
-        Button btnBitesPints    = findViewById(R.id.btnBitesPints);
-        Button btnBookshop      = findViewById(R.id.btnBookshop);
-        Button btnCafe          = findViewById(R.id.btnCafe);
-        Button btnHistorical    = findViewById(R.id.btnHistorical);
+        //Tour buttons
+        Button btnBitesPints = findViewById(R.id.btnBitesPints);
+        Button btnBookshop   = findViewById(R.id.btnBookshop);
+        Button btnCafe       = findViewById(R.id.btnCafe);
+        Button btnHistorical = findViewById(R.id.btnHistorical);
         Button btnNature = findViewById(R.id.btnNature);
 
         //Saved Tours button
         Button btnSavedTours = findViewById(R.id.btnSavedTours);
 
+        // When a button is clicked, get Tour from repository and send to ShowLocationInfoActivity
         btnBitesPints.setOnClickListener(
                 v -> openTour(ToursRepository.TOUR_BITES_PINTS)
         );
@@ -57,9 +61,8 @@ public class TourMenuActivity extends ComponentActivity {
         }
     }
 
-    // Opens the next screen with the selected Tour
-    private void openTour(String tourID) {
-        Tour tour = ToursRepository.getTour(tourID);
+    private void openTour(String tourId) {
+        Tour tour = ToursRepository.getTour(tourId);
         if (tour == null) return;
 
         Intent i = new Intent(this, ShowLocationInfoActivity.class);
